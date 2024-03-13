30f0f1244e45a035a137fb28255e1f49bf902875
GreatCode1989 <wef666wef6@gmail.com>
2024-03-13 18:50:24 +0300

    lo

--- js/main.js
+++ js/main.js
@@ -5,11 +5,22 @@ let sections = document.querySelectorAll('section')
 let navLinks = document.querySelectorAll('header nav a')
 
 window.addEventListener('scroll', () => {
-sections.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 100;
-    let height = sec.offsetHeight
-})
+  
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+    });
+
+    // Add active class to the appropriate navLink
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
+        }
+    });
 
     // sticky header
     let header = document.querySelector('header');
